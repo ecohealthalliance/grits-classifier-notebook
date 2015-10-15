29548bf2c1d8d922a19da963680a882187b5d7fd
Daniel Sullivan <danielalexandersullivan@gmail.com>
2015-10-15 16:01:23 -0400

    Check in notebook and required files

---
+++ config.py
@@ -0,0 +1,8 @@
+aws_access_key = 'AKIAJCUHF3G7PGECFHOQ'
+aws_secret_key = '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'
+
+BROKER_URL = 'mongodb://localhost:27017/tasks'
+mongo_url = 'localhost'
+
+bing_translate_id = "grits_api_test"
+bing_translate_secret = "+TPASOfHj+G8oEkS8IeKD4dkjeey8p6b5WKBYSnmMxM="
